35c83facdf8cead957b8479854782e22c89107c6
Jose A. Jimenez <jajimenezcarm@gmail.com>
2022-12-03 17:39:58 +0100

    Application initial structure added

---
+++ src/energy_es/__init__.py
@@ -0,0 +1,10 @@
+"""Energy-ES
+
+Energy-ES is a desktop application that displays the energy price for the
+current day in Spain. This application gets the data from a public API of Red
+Eléctrica.
+"""
+
+def main():
+    """Application main function."""
+    pass
