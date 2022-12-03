26622af5efe0889bd88e7c3efb958737277b3eb0
Jose A. Jimenez <jajimenezcarm@gmail.com>
2022-12-03 20:21:09 +0100

    Application/module version added

--- src/energy_es/__init__.py
+++ src/energy_es/__init__.py
@@ -1,9 +1,12 @@
-"""Energy-ES
+"""Energy-ES.
 
 Energy-ES is a desktop application that displays the energy price for the
 current day in Spain. This application gets the data from a public API of Red
 Eléctrica.
 """
+
+__version__ = "0.1.0"
+
 
 def main():
     """Application main function."""
